330d81ef57b6407e3821988d3cc7a02b49eee84b
eddie12994 <eamoros12@gmail.com>
2018-02-26 22:16:41 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server.');
 
-  socket.emit('createMessage', {
-    from: 'Edward',
-    text: 'That is fine.'
-  });
+  // socket.emit('createMessage', {
+  //   from: 'Edward',
+  //   text: 'That is fine.'
+  // });
 });
 
 socket.on('disconnect', function() {
