5b64816927260d257909f2b96330be513a7fd1c6
eddie12994 <eamoros12@gmail.com>
2018-03-14 00:48:16 -0400

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -46,8 +46,13 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    var user = users.getUser(socket.id);
+    // console.log('createMessage', message);
+
+    if(user && isRealString(message.text)){
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+
     callback();
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
@@ -57,9 +62,12 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createLocationMessage', (coords) => {
+    var user = users.getUser(socket.id);
     // io.emit('newMessage', generateMessage('Admin', `Latitude: ${coords.latitude}, Longitude: ${coords.longitude}`));
     // io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`));
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    if(user){
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));      
+    }
   });
 
   socket.on('disconnect', () => {
